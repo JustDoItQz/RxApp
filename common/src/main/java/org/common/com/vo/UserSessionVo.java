8fb1ca39a7133d5cb45c86172e09c4e9c3a7f370
JustDoItQz <1820066354@qq.com>
2017-09-18 15:58:23 +0800

    登录控制

---
+++ common/src/main/java/org/common/com/vo/UserSessionVo.java
@@ -0,0 +1,34 @@
+package org.common.com.vo;
+
+import java.io.Serializable;
+
+public class UserSessionVo implements Serializable {
+
+    private String username ;
+    private String password ;
+    private String loginType ;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoginType() {
+        return loginType;
+    }
+
+    public void setLoginType(String loginType) {
+        this.loginType = loginType;
+    }
+}
